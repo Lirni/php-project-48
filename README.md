1a79f1161cf85cd4daf340153c9df41a94aebfd2
Lirnie <L1rnend@yandex.by>
2026-03-24 11:31:22 +0300

    Step 4: Implement flat JSON diffing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Lirni/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Lirni/php-project-48/actions)
+
+[![asciicast](https://asciinema.org/a/UoIwDD2gb21I5Ep9.svg)](https://asciinema.org/a/UoIwDD2gb21I5Ep9)
--- src/Differ.php
+++ src/Differ.php
@@ -2,14 +2,50 @@
 
 namespace Hexlet\Code\Differ;
 
-function genDiff($pathToFile1, $pathToFile2)
+function toString(mixed $value): string
 {
-    $content1 = file_get_contents($pathToFile1);
-    $content2 = file_get_contents($pathToFile2);
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+    if (is_null($value)) {
+        return 'null';
+    }
+    return (string) $value;
+}
 
-    $data1 = json_decode($content1);
-    $data2 = json_decode($content2);
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $data1 = json_decode((string) file_get_contents($pathToFile1), true);
+    $data2 = json_decode((string) file_get_contents($pathToFile2), true);
 
-    print_r($data1);
-    print_r($data2);
+    $allKeys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
+    
+    $sortedKeys = $allKeys;
+    sort($sortedKeys);
+
+    $lines = array_map(function ($key) use ($data1, $data2) {
+        $existsIn1 = array_key_exists($key, $data1);
+        $existsIn2 = array_key_exists($key, $data2);
+
+        if ($existsIn1 && $existsIn2 && $data1[$key] === $data2[$key]) {
+            return "    {$key}: " . toString($data1[$key]);
+        }
+        
+        if ($existsIn1 && $existsIn2 && $data1[$key] !== $data2[$key]) {
+            $line1 = "  - {$key}: " . toString($data1[$key]);
+            $line2 = "  + {$key}: " . toString($data2[$key]);
+            return "{$line1}\n{$line2}";
+        }
+
+        if ($existsIn1 && !$existsIn2) {
+            return "  - {$key}: " . toString($data1[$key]);
+        }
+
+        return "  + {$key}: " . toString($data2[$key]);
+        
+    }, $sortedKeys);
+
+    $result = "{\n" . implode("\n", $lines) . "\n}";
+
+    return $result;
 }
